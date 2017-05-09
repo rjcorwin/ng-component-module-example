2c9b0cb8bd89da9e34274ba10cba7669d17711e2
R.J. Steinert <rj@rjsteinert.com>
2017-05-09 15:50:12 -0400

    Wire the Foo module to the App module so that App Component can use the Foo Component

--- src/app/foo/foo.module.ts
+++ src/app/foo/foo.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooComponent } from './foo/foo.component';
 
+export { FooComponent }
+
 @NgModule({
   imports: [
     CommonModule
   ],
+  exports: [FooComponent],
   declarations: [FooComponent]
 })
 export class FooModule { }
