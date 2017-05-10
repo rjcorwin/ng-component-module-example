d110ba9c73d5b646c69a9ef66c9d001e46167e35
R.J. Steinert <rj@rjsteinert.com>
2017-05-10 14:02:59 -0400

    Remove es6 export of component that we don't need

--- src/app/foo/foo.module.ts
+++ src/app/foo/foo.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooComponent } from './foo/foo.component';
 
-export { FooComponent }
-
 @NgModule({
   imports: [
     CommonModule
